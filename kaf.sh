f77b7b2e06f90cce1111ed2ecf3407d8ac930a21
Itamar Holtzman <itamar.holtman@gmail.com>
2024-11-23 14:30:36 +0200

    Preview services for topics

--- kaf.sh
+++ kaf.sh
@@ -30,14 +30,15 @@ else
 	esac
 fi
 
-TOPICS_LIST=$(gum spin --title "Fetching topics" -- curl -s "https://$DOMAIN/api/clusters/$CLUSTER/topics?&perPage=9999" | jq ".topics[].name")
+BASE_URL="https://$DOMAIN/api/clusters/$CLUSTER"
+TOPICS_LIST=$(gum spin --title "Fetching topics" -- curl -s "$BASE_URL/topics?&perPage=9999" | jq ".topics[].name" | tr -d '"')
 # If failed or empty
 if [ $? -ne 0 ] || [ -z "$TOPICS_LIST" ]; then
 	gum log -l error "Failed to fetch topics"
 	exit 1
 fi
 gum log -l debug "Topics count: $(echo "$TOPICS_LIST" | wc -l)"
-TOPIC=$(echo "$TOPICS_LIST" | gum filter --header "Choose topic:")
+TOPIC=$(echo "$TOPICS_LIST" | fzf --preview "curl -s $BASE_URL/topics/{}/consumer-groups | jq '.[].groupId' | sed 's/-consumer-group-server//g'")
 
 COMMAND="scripts/kafka/kafka-ui.py send --domain $DOMAIN --cluster $CLUSTER --topic $TOPIC --workers 10"
 echo $COMMAND
